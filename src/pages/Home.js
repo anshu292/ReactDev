3b0fdad63e33fabab8a5a55f497a2d6654ebf723
Anshu <anshugupta29a@gmail.com>
2024-09-16 12:12:16 +0530

    Used useRef in Home component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,28 @@
+import { useRef, useState } from "react"
+
 export default function Home(){
+
+    const[name, setName]= useState('some name');
+    const [nameList,setNameList]= useState(['fasd','adsd','ewew']);
+    const nameRef= useRef();
+
+    const printValue= ()=>{
+        console.log(nameRef.current.value);
+
+    }
 
     return(
         <>
         <h1>this is home component</h1>
-        </>
+        <h2>{name}</h2>
+        {
+        nameList.map((obj, index)=>{
+            return <h1>{obj}</h1>
+        })
+
+        }
+        <input ref={nameRef} type="text" placeholder="enter name"/>
+        <input onClick={()=> printValue()} type="button" value="Click"/>
+          </>
     )
 }
